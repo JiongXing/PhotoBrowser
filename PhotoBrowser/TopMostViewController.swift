a7d1be1b98303b5a70db335b10c6f08439bb12f1
JiongXing <549235261@qq.com>
2018-05-04 18:50:17 +0800

    增加取顶层 ViewController 方法

---
+++ PhotoBrowser/TopMostViewController.swift
@@ -0,0 +1,67 @@
+//
+//  TopMostViewController.swift
+//  JXPhotoBrowser
+//
+//  Created by JiongXing on 2018/5/4.
+//
+
+#if os(iOS) || os(tvOS)
+import UIKit
+
+class TopMostViewController {
+    private class var sharedApplication: UIApplication? {
+        let selector = NSSelectorFromString("sharedApplication")
+        return UIApplication.perform(selector)?.takeUnretainedValue() as? UIApplication
+    }
+    
+    /// Returns the current application's top most view controller.
+    open class var topMost: UIViewController? {
+        guard let currentWindows = self.sharedApplication?.windows else { return nil }
+        var rootViewController: UIViewController?
+        for window in currentWindows {
+            if let windowRootViewController = window.rootViewController {
+                rootViewController = windowRootViewController
+                break
+            }
+        }
+        
+        return self.topMost(of: rootViewController)
+    }
+    
+    /// Returns the top most view controller from given view controller's stack.
+    open class func topMost(of viewController: UIViewController?) -> UIViewController? {
+        // presented view controller
+        if let presentedViewController = viewController?.presentedViewController {
+            return self.topMost(of: presentedViewController)
+        }
+        
+        // UITabBarController
+        if let tabBarController = viewController as? UITabBarController,
+            let selectedViewController = tabBarController.selectedViewController {
+            return self.topMost(of: selectedViewController)
+        }
+        
+        // UINavigationController
+        if let navigationController = viewController as? UINavigationController,
+            let visibleViewController = navigationController.visibleViewController {
+            return self.topMost(of: visibleViewController)
+        }
+        
+        // UIPageController
+        if let pageViewController = viewController as? UIPageViewController,
+            pageViewController.viewControllers?.count == 1 {
+            return self.topMost(of: pageViewController.viewControllers?.first)
+        }
+        
+        // child view controller
+        for subview in viewController?.view?.subviews ?? [] {
+            if let childViewController = subview.next as? UIViewController {
+                return self.topMost(of: childViewController)
+            }
+        }
+        
+        return viewController
+    }
+}
+
+#endif
