303e83fb1d764c5f55b099979d904ba96d47efa2
梁炯幸 <liangjiongxing@qq.com>
2019-04-08 14:26:15 +0800

    [Fix]整理

--- Source/Core/JXCollectionViewFlowLayout.swift
+++ Source/Core/JXCollectionViewFlowLayout.swift
@@ -13,8 +13,10 @@ open class JXCollectionViewFlowLayout: UICollectionViewFlowLayout {
     
     open override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint) -> CGPoint {
         if let indexPath = self.indexPathForFocusItem {
-            if let layoutAttrs = self.layoutAttributesForItem(at: indexPath), let collectionView = self.collectionView {
-                return CGPoint(x: layoutAttrs.frame.origin.x - collectionView.contentInset.left, y: layoutAttrs.frame.origin.y - collectionView.contentInset.top)
+            if let layoutAttrs = self.layoutAttributesForItem(at: indexPath),
+                let collectionView = self.collectionView {
+                return CGPoint(x: layoutAttrs.frame.origin.x - collectionView.contentInset.left,
+                               y: layoutAttrs.frame.origin.y - collectionView.contentInset.top)
             }
             return super.targetContentOffset(forProposedContentOffset: proposedContentOffset)
         } else {
