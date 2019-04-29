237a094d051b282c8e34966814e8b8ef168af919
MangoJelly <luronghua1994@126.com>
2019-04-29 19:45:24 +0800

    Update LayoutUtil.swift
    更改RTL判断方法

--- Source/Core/Utils/LayoutUtil.swift
+++ Source/Core/Utils/LayoutUtil.swift
@@ -9,10 +9,6 @@ import UIKit
 
 class LayoutUtil: NSObject {
     public static var isRTLLayout: Bool {
-        guard let language = Bundle.main.preferredLocalizations.first else { return false } // en
-        if language.hasPrefix("ar") || language.hasPrefix("he") {
-            return true
-        }
-        return false
+        return UIApplication.shared.userInterfaceLayoutDirection == .rightToLeft
     }
 }
