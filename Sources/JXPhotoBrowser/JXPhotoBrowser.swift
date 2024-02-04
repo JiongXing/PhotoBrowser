19ad535c468b368e4835579df634e66318602bff
JiongXing <liangjiongxing@qq.com>
2024-02-04 20:11:10 +0800

    [fix] #221 取消对StatusBar的控制

--- Sources/JXPhotoBrowser/JXPhotoBrowser.swift
+++ Sources/JXPhotoBrowser/JXPhotoBrowser.swift
@@ -124,7 +124,6 @@ open class JXPhotoBrowser: UIViewController, UIViewControllerTransitioningDelega
         case .present(let fromVC, let embed):
             let toVC = embed?(self) ?? self
             toVC.modalPresentationStyle = .custom
-            toVC.modalPresentationCapturesStatusBarAppearance = true
             toVC.transitioningDelegate = self
             let from = fromVC ?? JXPhotoBrowser.topMost
             from?.present(toVC, animated: true, completion: nil)
@@ -211,37 +210,6 @@ open class JXPhotoBrowser: UIViewController, UIViewControllerTransitioningDelega
     }
     
     //
-    // MARK: - Status Bar
-    //
-    
-    private lazy var isPreviousStatusBarHidden: Bool = {
-        var previousVC: UIViewController?
-        if let vc = self.presentingViewController {
-            previousVC = vc
-        } else {
-            if let navVCs = self.navigationController?.viewControllers, navVCs.count >= 2 {
-                previousVC = navVCs[navVCs.count - 2]
-            }
-        }
-        return previousVC?.prefersStatusBarHidden ?? false
-    }()
-    
-    private lazy var isStatusBarHidden = self.isPreviousStatusBarHidden
-    
-    open override var prefersStatusBarHidden: Bool {
-        return isStatusBarHidden
-    }
-    
-    open func setStatusBar(hidden: Bool) {
-        if hidden {
-            isStatusBarHidden = true
-        } else {
-            isStatusBarHidden = isPreviousStatusBarHidden
-        }
-        setNeedsStatusBarAppearanceUpdate()
-    }
-    
-    //
     // MARK: - 转场
     //
     
@@ -266,7 +234,6 @@ open class JXPhotoBrowser: UIViewController, UIViewControllerTransitioningDelega
     
     /// 关闭PhotoBrowser
     open func dismiss() {
-        setStatusBar(hidden: false)
         pageIndicator?.removeFromSuperview()
         if presentingViewController != nil {
             dismiss(animated: true, completion: nil)
--- Sources/JXPhotoBrowser/JXPhotoBrowserImageCell.swift
+++ Sources/JXPhotoBrowser/JXPhotoBrowserImageCell.swift
@@ -245,7 +245,6 @@ open class JXPhotoBrowserImageCell: UIView, UIScrollViewDelegate, UIGestureRecog
             let result = panResult(pan)
             imageView.frame = result.frame
             photoBrowser?.maskView.alpha = result.scale * result.scale
-            photoBrowser?.setStatusBar(hidden: result.scale > 0.99)
             photoBrowser?.pageIndicator?.isHidden = result.scale < 0.99
         case .ended, .cancelled:
             imageView.frame = panResult(pan).frame
@@ -254,7 +253,6 @@ open class JXPhotoBrowserImageCell: UIView, UIScrollViewDelegate, UIGestureRecog
                 photoBrowser?.dismiss()
             } else {
                 photoBrowser?.maskView.alpha = 1.0
-                photoBrowser?.setStatusBar(hidden: true)
                 photoBrowser?.pageIndicator?.isHidden = false
                 resetImageViewPosition()
             }
